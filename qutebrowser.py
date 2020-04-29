6b76082659cb28bb8c4ba58bd14780e84e50da63
Tanner Hoelzel <thoelze1@binghamton.edu>
2020-04-29 15:18:50 -0400

    No window decoration

---
+++ qutebrowser.py
@@ -0,0 +1,5 @@
+# https://github.com/qutebrowser/qutebrowser/issues/3199
+# https://www.reddit.com/r/qutebrowser/comments/5wj3ku/hide_title_bar_in_macos/
+c.window.hide_decoration = True
+
+# https://github.com/qutebrowser/qutebrowser/blob/master/doc/help/configuring.asciidoc#configpy
