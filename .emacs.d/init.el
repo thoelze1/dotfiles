81828fb98386a58412f6b40264a1bd4f626a2d7a
Tanner Hoelzel <thoelze1@binghamton.edu>
2020-05-02 11:18:43 -0400

    Simplify startup appearance settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,3 @@
-;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
-;; No splash screen please ...jeez
-(setq inhibit-startup-message t)
-
 ;; Tell emacs where other config files are
 (setq site-lisp-dir
       (expand-file-name "site-lisp" user-emacs-directory)
--- .emacs.d/settings/appearance.el
+++ .emacs.d/settings/appearance.el
@@ -1,5 +1,16 @@
-;; Find a way to do this earlier in startup
+(setq initial-buffer-choice "~/git")
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
 (add-to-list 'default-frame-alist '(undecorated . t))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(font . "SF Mono-12"))
+
+;; set transparency
+;; https://lwn.net/Articles/88179/
+;; first value is for active frame, second value is for inactive frame
+(set-frame-parameter (selected-frame) 'alpha '(99 100))
 
 (provide 'appearance)
