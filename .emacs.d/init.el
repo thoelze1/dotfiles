fbeca083a5e4d78a8eb60720741b0c780949851c
Tanner Hoelzel <thoelze1@binghamton.edu>
2020-05-02 11:58:41 -0400

    Remove junk

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,8 +11,6 @@
 
 (package-initialize)
 
-(require 'package)
-
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
@@ -74,9 +72,3 @@
   (exec-path-from-shell-initialize))
 
 (require 'key-bindings)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
