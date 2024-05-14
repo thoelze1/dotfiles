4f3e37180c252e5e6cc9a8ac22fc8bb779f982eb
Tanner Hoelzel <thoelze1@binghamton.edu>
2024-05-13 23:03:04 -0400

    custom.el
    This file hasn't been loaded by default during init for a long time (I
    never have line numbers and package-selected-packages currently
    evaluates to nil). I'm just commiting it for posterity

---
+++ .emacs.d/custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-line-numbers-exempt-modes
+   '(vterm-mode eshell-mode shell-mode term-mode ansi-term-mode dired-mode helm-mode))
+ '(display-line-numbers-width nil)
+ '(display-line-numbers-width-start nil)
+ '(epg-gpg-program "/usr/local/bin/gpg")
+ '(indicate-buffer-boundaries '((t)))
+ '(indicate-empty-lines nil)
+ '(overflow-newline-into-fringe nil)
+ '(package-selected-packages
+   '(lsp-mode dotenv-mode dtrt-indent treemacs ag helm-ag helm-projectile floobits neotree centaur-tabs zenburn-theme yasnippet xelb web-mode use-package typescript-mode ssass-mode slime sicp rainbow-delimiters projectile ox-twbs osx-plist org-roam org-babel-eval-in-repl multiple-cursors mmm-mode magit ledger-mode julia-mode htmlize html-to-hiccup helm-swoop helm-org haskell-mode graphql ghub geiser-guile exec-path-from-shell elm-mode edit-indirect company cider)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-line-numbers ((t :weight bold)))
+ '(fringe ((t (:background "#383838" :foreground "#DCDCCC"))))
+ '(line-number ((t (:background "#383838" :foreground "#6F6F6F" :width ultra-condensed))))
+ '(window-divider ((t (:foreground "gray60" :width condensed)))))
